511c069518a268720b1a6c17a07fb959cbbdd49e
danielmengte <danielmengoteessu@gmail.com>
2025-08-11 12:54:17 +0800

    Portolio Project Trial

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Navbar } from './shared/components/navbar/navbar';
+import { Home } from './pages/home/home';
+import { Projects } from "./pages/projects/projects";
+import { Footer } from "./shared/components/footer/footer";
+// import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Navbar, Home, Projects, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
